7527dd23bc79cfc308325a5c538b7c43bf871831
Joyce <fogangbj@3il.fr>
2025-04-02 12:53:57 +0200

    Tuto 05

--- src/main/java/tuto/web/WebConnexion.java
+++ src/main/java/tuto/web/WebConnexion.java
@@ -3,8 +3,13 @@ package tuto.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@SessionAttributes( "connected" )
 public class WebConnexion {
 
     @GetMapping("/bonjour")
@@ -12,15 +17,28 @@ public class WebConnexion {
         return "bonjour";
     }
 
-    @GetMapping("/connect")
+    @PostMapping("/connect")
     public String connect(String username, String password, Model model) {
         var sb = new StringBuilder(username);
+        
         if (sb.reverse().toString().equals(password)) {
             model.addAttribute("connected", username);
-            return "bonjour";
+            return "redirect:/bonjour";
         } else {
             model.addAttribute("alert", "Mot de passe incorrect");
+            model.addAttribute("username", username); // Conserve le nom d'utilisateur saisi
+            model.addAttribute("password", password); // Conserve le mot de passe saisi
             return "index";
         }
     }
+
+
+
+
+     @PostMapping("/disconnect")
+     public String disconnect(SessionStatus status, Model model, RedirectAttributes ra) {
+    	 status.setComplete();
+    	 ra.addFlashAttribute(  "alert", "Déconnexion effectuée avec succès" );
+    	 return "redirect:/";
+       }
 }
